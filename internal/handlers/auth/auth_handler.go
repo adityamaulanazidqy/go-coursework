b4b2eb7ecc1b34890c576aba4ac93eff9d78f5fb
Aditya Maulana zidqy <adityamaullana234@gmail.com>
2025-05-24 02:27:31 +0700

    Complete authentication, namely logout

--- internal/handlers/auth/auth_handler.go
+++ internal/handlers/auth/auth_handler.go
@@ -1,22 +1,27 @@
 package auth
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 	"go-coursework/internal/dto/auth"
 	"go-coursework/internal/helpers"
 	"go-coursework/internal/logger"
 	"go-coursework/internal/repositories"
+	"go-coursework/pkg/jwt"
 	"gorm.io/gorm"
+	"time"
 )
 
 type AuthenticationHandler struct {
-	db        *gorm.DB
-	logLogrus *logger.ErrorLogger
-	authRepo  *repositories.AuthenticationRepo
+	db          *gorm.DB
+	logLogrus   *logger.ErrorLogger
+	redisClient *redis.Client
+	authRepo    *repositories.AuthenticationRepo
 }
 
-func NewAuthenticationHandler(db *gorm.DB, logLogrus *logger.ErrorLogger) *AuthenticationHandler {
-	return &AuthenticationHandler{db: db, logLogrus: logLogrus, authRepo: repositories.NewAuthenticationRepo(db)}
+func NewAuthenticationHandler(db *gorm.DB, logLogrus *logger.ErrorLogger, redisClient *redis.Client) *AuthenticationHandler {
+	return &AuthenticationHandler{db: db, logLogrus: logLogrus, redisClient: redisClient, authRepo: repositories.NewAuthenticationRepo(db)}
 }
 
 func (h *AuthenticationHandler) SignIn(ctx *fiber.Ctx) error {
@@ -82,5 +87,48 @@ func (h *AuthenticationHandler) SignUp(ctx *fiber.Ctx) error {
 	return ctx.Status(code).JSON(fiber.Map{
 		"message": msg,
 		"data":    resp,
+	})
+}
+
+func (h *AuthenticationHandler) Logout(ctx *fiber.Ctx) error {
+	const op = "handler.AuthHandler.Logout"
+
+	var (
+		msgTokenError                 = "Failed to parse token"
+		msgTokenErrorDetails          = []string{"Maybe you haven't logged in yet"}
+		msgTokenFailedVerify          = "Token verification failed"
+		msgTokenFailedVerifyDetails   = []string{"There was an error with your token. Please re-login to get a new access token"}
+		msgInternalServerError        = "Internal Server Error"
+		msgInternalServerErrorDetails = []string{"User Internal Server Error", "Make sure your internet is on", "There may be a problem with our server"}
+	)
+
+	token, err := jwt.ExtractTokenFromHeader(ctx)
+	if err != nil {
+		h.logLogrus.LogUserError("-", err, msgTokenError)
+		return h.logLogrus.LogRequestError(ctx, fiber.StatusUnauthorized, op, err, msgTokenError, msgTokenErrorDetails)
+	}
+
+	claims, err := jwt.VerifyToken(token)
+	if err != nil {
+		h.logLogrus.LogUserError("-", err, msgTokenFailedVerify)
+		return h.logLogrus.LogRequestError(ctx, fiber.StatusUnauthorized, op, err, msgTokenFailedVerify, msgTokenFailedVerifyDetails)
+	}
+
+	expDuration := time.Until(claims.ExpiresAt.Time)
+	if expDuration <= 0 {
+		expDuration = time.Minute * 1
+	}
+
+	ctxBackground := context.Background()
+	err = h.redisClient.Set(ctxBackground, "blacklist:"+token, "true", expDuration).Err()
+	if err != nil {
+		h.logLogrus.LogUserError("-", err, msgInternalServerError)
+		return h.logLogrus.LogRequestError(ctx, fiber.StatusInternalServerError, op, err, msgInternalServerError, msgInternalServerErrorDetails)
+	}
+
+	h.logLogrus.Logger.Info(ctx.IP() + " => User successfully logged out.")
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Successfully logged out.",
 	})
 }
--- internal/routes/auth/routes.go
+++ internal/routes/auth/routes.go
@@ -4,13 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go-coursework/internal/handlers/auth"
 	"go-coursework/internal/models"
+	"go-coursework/pkg/jwt"
 )
 
 func Setup(router fiber.Router, ctx *models.RouterContext) {
-	controller := auth.NewAuthenticationHandler(ctx.DB, ctx.Logger)
+	controller := auth.NewAuthenticationHandler(ctx.DB, ctx.Logger, ctx.RedisClient)
 	authGroup := router.Group("/auth")
 	{
 		authGroup.Post("/signup", controller.SignUp)
 		authGroup.Post("/signin", controller.SignIn)
+		authGroup.Post("/logout", jwt.Middleware("Admin", "Lecturer", "Student"), controller.Logout)
 	}
 }
