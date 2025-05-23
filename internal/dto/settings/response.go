ed5f53b388e534b607308cf0bdfdb27c8eb9b345
Aditya Maulana zidqy <adityamaullana234@gmail.com>
2025-05-24 05:29:34 +0700

    Adding Set Profile and Phone Number Features

---
+++ internal/dto/settings/response.go
@@ -0,0 +1,12 @@
+package settings
+
+type SetResponse struct {
+	ID           int     `json:"-"`
+	Username     string  `json:"username"`
+	Email        string  `json:"email"`
+	Telephone    *string `json:"telephone"`
+	StudyProgram string  `json:"study_program"`
+	Role         string  `json:"role"`
+	Batch        int     `json:"batch"`
+	Profile      *string `json:"profile"`
+}
